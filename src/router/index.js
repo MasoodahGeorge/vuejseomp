f7594e1c73d83c03807b4fac683fd3a8a8ee38d1
MasoodahGeorge <oodasbusiness@gmail.com>
2024-07-05 14:01:01 +0200

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,58 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import ResumeView from '../views/ResumeView.vue';
+import SkillsView from '../views/SkillsView.vue';
+import TestimonialsView from '../views/TestimonialsView.vue'
+import ProjectsView from '../views/ProjectsView.vue'
+import ContactView from '../views/ContactView.vue'
+import FooterComp from '../components/FooterComp.vue';
+// import NavbarComp from '../components/NavbarComp.vue';
 
 const routes = [
   {
+    // path: '/',
+    // component: Layout,
+    // children: [
+    //   {
+    //     path: '',
+    //     name: 'home',
+    //     component: HomeView,
+    //   },
+    //   {
+    //     path: 'about',
+    //     name: 'about',
+    //     component: AboutView,
+    //   },
+    //   {
+    //     path: 'resume',
+    //     name: 'resume',
+    //     component: ResumeView,
+    //   },
+    //   {
+    //     path: 'skills',
+    //     name: 'skills',
+    //     component: SkillsView,
+    //   },
+    // ],
     path: '/',
-    name: 'home',
-    component: HomeView
+    components: {
+      default: HomeView,
+      about: AboutView,
+      resume: ResumeView,
+      skills: SkillsView,
+      testimonials:TestimonialsView,
+      projects:ProjectsView,
+      contact:ContactView,
+      FooterComp:FooterComp
+    }
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
